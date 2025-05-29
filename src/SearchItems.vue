3eb52f50e46503e512579ec817de02761d7879d8
mymt0403 <kaneking0703@gmail.com>
2025-05-29 17:56:53 +0900

    add draggable pin and route info

--- src/SearchItems.vue
+++ src/SearchItems.vue
@@ -2,19 +2,40 @@
 import { ref, defineEmits, onMounted } from 'vue';
 import axios from 'axios';
 
-let map
-const tokyoLat = parseFloat(import.meta.env.VITE_DEFAULT_TOKYO_LAT)
-const tokyoLng = parseFloat(import.meta.env.VITE_DEFAULT_TOKYO_LNG)
+let map, userLocationMarker
 const emit = defineEmits([`sendData`])
 const selectedValue = ref('40')
+const url = 'https://routes.googleapis.com/distanceMatrix/v2:computeRouteMatrix'
+const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+const userPosition = {}
 
-async function fetchCenter() {
+async function init() {
+    const facilities = await fetchFacilitiesInfo()
+    await putPins(facilities)
+    putDragablePin()
+    emit(`sendData`, facilities)
+}
+
+async function search() {
+    const facilities = await fetchFacilitiesInfo()
+    await putPins(facilities)
+    putDragablePin()
+    const enrichedFacilities = await getRouteInfo(userPosition, facilities)
+    emit(`sendData`, enrichedFacilities)
+}
+
+async function fetchFacilitiesInfo() {
     const { Map } = await window.google.maps.importLibrary('maps');
 
     /** 中央座標を取得し、マップを表示する */
     await axios.get(`http://localhost:8080/api/center/${selectedValue.value}`)
         .then(
             function (response) {
+                if(!userPosition.lat && !userPosition.lng) {
+                    userPosition.lat = response.data.latitude
+                    userPosition.lng = response.data.longitude
+                }
+
                 map = new Map(document.getElementById("map"), {
                     center: {
                         lat: response.data.latitude,
@@ -26,13 +47,9 @@ async function fetchCenter() {
             }
         )
 
-        /** 選択した都道府県の施設情報を取得し、マップ上にピンを指す */
-    await axios.get(`http://localhost:8080/api/data/${selectedValue.value}`)
-        .then(
-            function (response) {
-                putPins(response.data)
-            }
-        )
+    /** 選択した都道府県の施設情報を取得し、マップ上にピンを指す */
+    const facilitiesRes = await axios.get(`http://localhost:8080/api/data/${selectedValue.value}`)
+    return facilitiesRes.data
 }
 
 async function putPins(pins) {
@@ -52,61 +69,148 @@ async function putPins(pins) {
             }).element,
         });
     });
-    emit(`sendData`, pins)
 }
 
-function showUserLocation() {
-    const { AdvancedMarkerElement } = google.maps.importLibrary("marker");
-    const { PinElement } = google.maps.importLibrary("marker");
+function displayUserLocation() {
+    /**  Font Awesome のアイコンを使用する */
+    const icon = document.createElement("div");
+    icon.innerHTML = '<i class="fa-solid fa-user"></i>';
+
+    const faPin = new google.maps.marker.PinElement({
+        glyph: icon,
+        glyphColor: "#FFFFFF",
+        background: "#0000FF",
+        borderColor: "#FFFFFF",
+    });
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
             (position) => {
-                const pos = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude,
-                };
+                const errorMessageElement = document.getElementById("errorMessage")
+                errorMessageElement.style.display = "none"
 
-               /**  Font Awesome のアイコンを使用する */
-                const icon = document.createElement("div");
-                icon.innerHTML = '<i class="fa-solid fa-user"></i>';
+                // const userPosition = {
+                //     lat: position.coords.latitude,
+                //     lng: position.coords.longitude,
+                // };
 
-                const faPin = new google.maps.marker.PinElement({
-                    glyph: icon,
-                    glyphColor: "#000000",
-                    background: "#FF6633",
-                    borderColor: "#FF0000",
-                });
+                /** 仮の現在地 */
+                userPosition.lat = 33.747585595093774
+                userPosition.lng = 130.64076497636538
+
+                userLocationMarker.position = null
 
                 /** 現在地をマークする */
-                new google.maps.marker.AdvancedMarkerElement({
+                userLocationMarker = new google.maps.marker.AdvancedMarkerElement({
                     map,
-                    position: pos,
+                    position: userPosition,
+                    gmpDraggable: true,
                     content: faPin.element,
                     title: "your location",
                 });
 
-                map.setCenter(pos);
+                map.setCenter(userPosition);
             },
             () => {
-                handleLocationError();
+                handleLocationError()
             },
         );
     } else {
-        handleLocationError();
+        handleLocationError()
     }
 }
 
 /** 初期表示時に検索を実行 */
 onMounted(() => {
-    fetchCenter()
+    init()
 })
+
+async function getRouteInfo(position, pins) {
+    const payload = {
+        origins: [
+            {
+                waypoint: {
+                    location: {
+                        latLng: {
+                            latitude: position.lat,
+                            longitude: position.lng
+                        }
+                    }
+                },
+            },
+        ],
+        destinations: pins.map(dest => ({
+            waypoint: {
+                location: {
+                    latLng: {
+                        latitude: dest.latitude,
+                        longitude: dest.longitude
+                    }
+                }
+            }
+        })),
+        travelMode: 'DRIVE',
+        routingPreference: 'TRAFFIC_AWARE'
+    }
+
+    const headers = {
+        'Content-Type': 'application/json',
+        'X-Goog-Api-Key': apiKey,
+        'X-Goog-FieldMask': 'originIndex,destinationIndex,duration,distanceMeters,status,condition'
+    }
+
+    const response = await axios.post(url, payload, { headers })
+    return pins.map((pin, index) => {
+        const match = response.data.find(d => d.destinationIndex === index);
+        return {
+            ...pin,
+            duration: match ? match.duration : null,
+            distanceMeters: match ? match.distanceMeters : null,
+        };
+    });
+}
+
+function putDragablePin () {
+    const infoWindow = new google.maps.InfoWindow();
+
+    /**  Font Awesome のアイコンを使用する */
+    const icon = document.createElement("div");
+    icon.innerHTML = '<i class="fa-solid fa-user"></i>';
+
+    const faPin = new google.maps.marker.PinElement({
+        glyph: icon,
+        glyphColor: "#FFFFFF",
+        background: "#0000FF",
+        borderColor: "#FFFFFF",
+    });
+
+    userLocationMarker = new google.maps.marker.AdvancedMarkerElement({
+        map,
+        position: { lat: userPosition.lat, lng: userPosition.lng },
+        gmpDraggable: true,
+        content: faPin.element,
+        title: "This marker is draggable.",
+    });
+    infoWindow.setContent(`出発地点まで動かして「search」を押してください。`);
+    infoWindow.open(userLocationMarker.map, userLocationMarker);
+
+    userLocationMarker.addListener("dragstart", (event) => {
+        infoWindow.close();
+    });
+
+    userLocationMarker.addListener("dragend", (event) => {
+        const position = userLocationMarker.position;
+        userPosition.lat = position.lat
+        userPosition.lng = position.lng
+        console.log(`ピンの座標：${userPosition.lat}, ${userPosition.lng}`)
+    });
+}
 
 function handleLocationError() {
     const errorMessageElement = document.getElementById("errorMessage");
     errorMessageElement.innerHTML = `
         <p>位置情報が取得できませんでした。<br>
-        位置情報を表示させる場合はブラウザの設定から位置情報へのアクセスを許可してください。</p>`;
+        所要時間と走行距離を表示させる場合は、ブラウザの設定から位置情報へのアクセスを許可してください。</p>`;
     errorMessageElement.style.display = "block";
 }
 </script>
@@ -123,8 +227,9 @@ function handleLocationError() {
             <option value="46">鹿児島県</option>
             <option value="47">沖縄県</option>
         </select>
-        <button class="button1" @click="fetchCenter">search</button>
-        <button class="button2" @click="showUserLocation">display your location</button>
+        <button class="button1" @click="search">search</button>
+        <button class="button2" @click="displayUserLocation">現在地を表示</button>
+
     </div>
     <div class="error" style="vertical-align: top;">
         <div id="errorMessage" class="error-message"></div>
@@ -156,9 +261,17 @@ function handleLocationError() {
 
 .button2 {
     padding: 5px 10px;
-    width: 200px;
+    width: 220px;
     height: 40px;
     font-size: 16px;
+}
+
+.button3 {
+    padding: 5px 10px;
+    width: 140px;
+    height: 40px;
+    font-size: 16px;
+    display: none;
 }
 
 .error-message {
